cfff6a56d45ec6d5e3dd807e26cb0b7075623651
nemobaji <khw175@naver.com>
2025-12-12 02:11:23 +0900

    Refactor: Convert to multi-threaded architecture

--- server.c
+++ server.c
@@ -4,11 +4,13 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <pthread.h>
 
 #define BUFFER_SIZE 1024
 #define PORT 8080
 
 void error_handling(char *message);
+void *handle_client(void *arg);
 
 int main(int argc, char *argv[]) {
     int server_sock;
@@ -17,9 +19,7 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
     socklen_t client_addr_size;
-
-    char message[BUFFER_SIZE];
-    int str_len;
+    pthread_t thread_id;
 
     server_sock = socket(PF_INET, SOCK_STREAM, 0);
     if (server_sock == -1) 
@@ -37,22 +37,48 @@ int main(int argc, char *argv[]) {
         error_handling("> listen() error");
     printf("> Echo server started. Waiting for connection on port %d...\n", PORT);
 
-    client_addr_size = sizeof(client_addr);
-    client_sock = accept(server_sock, (struct sockaddr*) &client_addr, &client_addr_size);
-    if(client_sock == -1)
-        error_handling("> accept() error");
-    printf("> Client connected. IP: %s\n", inet_ntoa(client_addr.sin_addr));
+    while (1) {
+        client_addr_size = sizeof(client_addr);
+        client_sock = accept(server_sock, (struct sockaddr*) &client_addr, &client_addr_size);
+        if(client_sock == -1)
+            error_handling("> accept() error");
+        
+        printf("> Client connected. IP: %s\n", inet_ntoa(client_addr.sin_addr));
 
-    while((str_len = read(client_sock, message, BUFFER_SIZE - 1)) != 0) {
-        write(client_sock, message, str_len);
-        message[str_len] = 0;
-        printf("> Received and echoed: %s", message);
+        int *new_sock = (int*)malloc(sizeof(int));
+        *new_sock = client_sock;
+
+        if (pthread_create(&thread_id, NULL, handle_client, (void*) new_sock) != 0) {
+            close(client_sock);
+            free(new_sock);
+            fputs("> pthread_create() error\n", stderr);
+            continue;
+        }
+
+        pthread_detach(thread_id);
+        printf("> [Main Thread] Forked thread ID %lu to handle client. \n", (unsigned long)thread_id);
     }
 
     close(server_sock);
-    close(client_sock);
 
     return 0;
+}
+
+void *handle_client(void *arg) {
+    int client_sock = *((int*)arg);
+    free(arg);
+
+    char message[BUFFER_SIZE];
+    int str_len;
+
+    while((str_len = read(client_sock, message, BUFFER_SIZE)) != 0) {
+        write(client_sock, message, str_len);
+    }
+
+    close(client_sock);
+    printf("> [Thread %lu] Client disconnected. Thread terminated.\n", (unsigned long)pthread_self());
+
+    return NULL;
 }
 
 void error_handling(char *message) {
